53febe27083c266e8e8a1abf5b639cd04a51c6e6
Charles <charlesneri0201@gmail.com>
2025-09-04 21:14:33 +0800

    need nag design and database for other implimentation

--- src/mainsite/HomepageView.vue
+++ src/mainsite/HomepageView.vue
@@ -59,7 +59,7 @@ onMounted(async () => {
     <!-- Main Content -->
     <v-main class="app-main">
       <v-card class="mx-auto my-8 pa-6" color="primary" elevation="2" max-width="400">
-        <h1 class="text-h4 text-center text-white font-weight-bold">second</h1>
+        <h1 class="text-h4 text-center text-white font-weight-bold">wapay content waiting for the database</h1>
       </v-card>
     </v-main>
 
