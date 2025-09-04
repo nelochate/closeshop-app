cd0064fcb6ee7770bd7cc69d27566ed01cea6f36
Charles <charlesneri0201@gmail.com>
2025-09-04 09:19:59 +0800

    adding file for now

---
+++ src/mainsite/ProfileView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="notification-view">
+    <h1>profile</h1>
+    <p>No notifications yet.</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+// you can add logic later here
+</script>
+
+<style scoped>
+.notification-view {
+  padding: 16px;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,10 @@ import LoginView from '../auth/LoginView.vue'
 import RegisterView from '@/auth/RegisterView.vue'
 import HomepageView from '@/mainsite/HomepageView.vue'
 import MapSearch from '@/mainsite/MapSearch.vue'
+import CartView from '@/mainsite/CartView.vue'
+import MsessagevVew from '@/mainsite/MsessagevVew.vue'
+import ProfileView from '@/mainsite/ProfileView.vue'
+import NotificationView from '@/mainsite/NotificationView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -26,7 +30,28 @@ const router = createRouter({
       path: '/mapsearch',
       name: 'mapsearch',
       component: MapSearch
+    },
+    {
+      path: '/cartview',
+      name: 'cartview',
+      component: CartView
+    },
+     {
+      path: '/messageview',
+      name: 'messageview',
+      component: MsessagevVew
+    },
+     {
+      path: '/profileview',
+      name: 'profileview',
+      component: ProfileView
+    },
+    {
+      path: '/notificationview',
+      name: 'notificationview',
+      component: NotificationView
     }
+
   ],
 })
 
