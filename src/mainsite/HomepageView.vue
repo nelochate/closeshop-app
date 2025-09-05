5d07af7c919ce29396bf3756b3d7ee88687b2f5f
Nel Ochate <ochatenel20@gmail.com>
2025-09-05 21:29:42 +0800

    feat: registration success confirm email message

--- src/mainsite/HomepageView.vue
+++ src/mainsite/HomepageView.vue
@@ -41,48 +41,32 @@ const handleLogout = async () => {
 <template>
   <v-app>
     <!-- Top Navigation -->
-    <v-app-bar
-      class="top-nav "
-      elevation="0"
-      flat
-      color="#5ca3eb"
-
-    >
-     <div class="m-10">
-       <v-text-field
-        v-model="search"
-        label="Search..."
-        hide-details
-        density="comfortable"
-        variant="outlined"
-        class="search-bar"
-        @keyup.enter="onSearch"
-      >
-        <template v-slot:append>
-          <v-btn icon @click="onSearch">
-            <v-icon>mdi-magnify</v-icon>
-          </v-btn>
-          <v-btn icon to="/mapsearch">
-            <v-icon>mdi-search-web</v-icon>
-          </v-btn>
-        </template>
-      </v-text-field>
-     </div>
+    <v-app-bar class="top-nav " elevation="0" flat color="#5ca3eb">
+      <div class="m-10">
+        <v-text-field v-model="search" label="Search..." hide-details density="comfortable" variant="outlined"
+          class="search-bar" @keyup.enter="onSearch">
+          <template v-slot:append>
+            <v-btn icon @click="onSearch">
+              <v-icon>mdi-magnify</v-icon>
+            </v-btn>
+            <v-btn icon to="/mapsearch">
+              <v-icon>mdi-search-web</v-icon>
+            </v-btn>
+          </template>
+        </v-text-field>
+      </div>
     </v-app-bar>
 
     <!-- Main Content -->
     <v-main class="app-main">
-    <v-card class="mx-auto my-8 pa-6" color="primary" elevation="2" max-width="400">
-  <h1 class="text-h4 text-center text-white font-weight-bold">second</h1>
-</v-card>
-<v-btn color="error" @click="handleLogout">Logout</v-btn>
+      <v-card class="mx-auto my-8 pa-6" color="primary" elevation="2" max-width="400">
+        <h1 class="text-h4 text-center text-white font-weight-bold">second</h1>
+      </v-card>
+      <v-btn color="error" @click="handleLogout">Logout</v-btn>
     </v-main>
 
     <!-- Bottom Navigation -->
-    <v-bottom-navigation
-      class="bot-nav"
-      height="64"
-    >
+    <v-bottom-navigation class="bot-nav" height="64">
       <v-btn value="home" @click="goHome">
         <v-icon>mdi-home-outline</v-icon>
       </v-btn>
